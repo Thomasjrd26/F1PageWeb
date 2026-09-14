2558b17f8c1e1855d40f232d6c8a7bdd45ad029c
Thomas <91837234+Thomasjrd26@users.noreply.github.com>
2026-09-14 17:38:53 +0200

    Create create_zip.py

---
+++ create_zip.py
@@ -0,0 +1,7 @@
+import zipfile
+
+with zipfile.ZipFile("paddock_f1_2026_v14.zip", "w", zipfile.ZIP_DEFLATED) as zip_file:
+    for f in ["index.html", "style.css", "app.js"]:
+        zip_file.write(f)
+
+print("Zip généré !")
